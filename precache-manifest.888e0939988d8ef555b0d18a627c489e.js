e129f84ce2a61e2b24332b965774ed68caf91f6b
Sara Fedlu <sfedlu28@gmail.com>
2024-07-18 12:51:22 +0300

    Deploy to GitHub Pages

---
+++ precache-manifest.888e0939988d8ef555b0d18a627c489e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08660adb76df5a952675f048dd0b80d2",
+    "url": "/Interactive_data_explorer/frontend/index.html"
+  },
+  {
+    "revision": "31f522960fb5653b5f1f",
+    "url": "/Interactive_data_explorer/frontend/static/css/main.de6021f3.chunk.css"
+  },
+  {
+    "revision": "e3a5b9b844ca6f0e8bd6",
+    "url": "/Interactive_data_explorer/frontend/static/js/2.0984b314.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/Interactive_data_explorer/frontend/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "31f522960fb5653b5f1f",
+    "url": "/Interactive_data_explorer/frontend/static/js/main.2fbe7081.chunk.js"
+  },
+  {
+    "revision": "d7c26b6d00e72f7a3b2c",
+    "url": "/Interactive_data_explorer/frontend/static/js/runtime~main.0addb730.js"
+  }
+]);
